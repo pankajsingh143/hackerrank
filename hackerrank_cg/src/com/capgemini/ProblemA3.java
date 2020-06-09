8ef322bb857c9c3806b9138b04440a3162a1224f
Pankaj Singh <pankajsingh143@gmail.com>
2020-06-09 17:59:48 +0530

    updating solution A3

--- hackerrank_cg/src/com/capgemini/ProblemA3.java
+++ hackerrank_cg/src/com/capgemini/ProblemA3.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class ProblemA3 {
 
 	public static void main(String[] args) {
-		int ar[] = {2, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5};
+		int ar[] = {2, 2, 3, 3, 3, 4, 4};
 		System.out.println(ar[ar.length - 1] + "  " + (ar.length - 1));
 		getMaxcount(ar);
 	}
@@ -15,6 +15,7 @@ public class ProblemA3 {
 		int index = 0;
 		int result = -1;
 		int mid = a.length / 2;
+		System.out.println("mid " + mid);
 		Map<Integer, Integer> countMap = new HashMap<>();
 		for (int i = 0; i <= a.length - 2; i++) {
 			if (a[i] == a[i + 1]) {
@@ -26,11 +27,26 @@ public class ProblemA3 {
 				count = 0;
 			}
 		}
-		System.out.print(index);
+		// System.out.print(index);
 
 		System.out.print(countMap);
+		int max = 0;
 		for (Integer s : countMap.keySet()) {
-			// if
+			// max = countMap.get(s);
+			System.out.println("max inside for " + max);
+			if (countMap.get(s) > max) {
+				System.out.println("countMap.get(s) " + countMap.get(s));
+				System.out.println("countMap s " + s);
+
+				max = countMap.get(s);
+				index = s;
+			}
+
+		}
+		System.out.println("max " + max);
+
+		if (max > mid) {
+			result = index;
 		}
 		System.out.println(result);
 		return result;
