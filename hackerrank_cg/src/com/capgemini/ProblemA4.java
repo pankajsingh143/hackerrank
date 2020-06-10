4827829f0fc3969aa80d3294096ac277bf43dd27
Pankaj Singh <pankajsingh143@gmail.com>
2020-06-10 10:29:15 +0530

    Adding solution A4

--- hackerrank_cg/src/com/capgemini/ProblemA4.java
+++ hackerrank_cg/src/com/capgemini/ProblemA4.java
@@ -1,7 +1,9 @@
 package com.capgemini;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class ProblemA4 {
 
@@ -12,25 +14,41 @@ public class ProblemA4 {
 	public static void getCount(int[] a) {
 		int sum = 0;
 		String pair = "";
-		Map<String, Integer> sumMap = new HashMap<>();
+		Map<Integer, String> sumMap = new HashMap<>();
 		for (int i = 0; i < a.length; i++) {
-			System.out.println("sum " + sum);
+			// System.out.println("sum " + sum);
 
 			for (int j = 1; j < a.length; j++) {
-				if (i == j)
-					continue;
+
 				sum = Math.abs(a[i] + a[j]);
 				pair = i + "," + j;
-				sumMap.put(pair, sum);
+				sumMap.put(sum, pair);
 
-				System.out.printf("pair (%d, %d) and sum  %d %n %s", i, j, sum,
-						pair);
+				// System.out.printf("pair (%d, %d) and sum %d %n %s", i, j,
+				// sum,
+				// pair);
 			}
 		}
 		System.out.println(sumMap);
-		for (String s : sumMap.keySet()) {
-			/// if(sumMap.get(s))
+		int min = 1;
+
+		Set<Integer> minSet = new HashSet<>();
+		for (Integer n : sumMap.keySet()) {
+			// min = sumMap.get(s);
+			// System.out.println("min " + min);
+			if (min > n) {
+				min = n;
+				System.out.printf("min %d s %d", min, n);
+			}
+			minSet.add(min);
+			if (min == n)
+				minSet.add(n);
 		}
+		System.out.println(minSet);
+		for (int n : minSet) {
+			System.out.printf("min %d pair %s ", n, sumMap.get(n));
+		}
+
 	}
 
 }
