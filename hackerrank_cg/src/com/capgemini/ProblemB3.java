2eb0a36ea8c2b643014757157f536b2dd5016ef8
Pankaj Singh <pankajsingh143@gmail.com>
2020-06-11 20:43:23 +0530

    Adding B3

--- hackerrank_cg/src/com/capgemini/ProblemB3.java
+++ hackerrank_cg/src/com/capgemini/ProblemB3.java
@@ -4,7 +4,7 @@ import java.util.StringJoiner;
 
 public class ProblemB3 {
 	public static void main(String... s) {
-		String str = "good  good bye day good";
+		String str = "good  good bye day good day";
 		String[] sar = str.split(" ");
 		// System.out.println(Arrays.toString(sar));
 		StringJoiner strj = new StringJoiner(" ");
@@ -12,6 +12,8 @@ public class ProblemB3 {
 			int index = 0;
 			if (str.indexOf(item) != str.lastIndexOf(item)) {
 				// System.out.println("duplicate " + item);
+				if (strj.toString().indexOf(item) < 0)
+					strj.add(item);
 				item = "";
 			}
 			strj.add(item);
